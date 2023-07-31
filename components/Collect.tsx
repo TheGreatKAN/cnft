f826acc73e9843b1e2317f8cf6665693843497b8
TheGreatKAN <95988106+TheGreatKAN@users.noreply.github.com>
2023-07-31 02:55:42 -0400

    Update Collect.tsx

--- components/Collect.tsx
+++ components/Collect.tsx
@@ -1,9 +1,12 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { ethers, formatEther } from 'ethers';
 import nft from '../images/nftPrev.png'
 import rewardsLogo from '../images/rewardsLogo.png'
 import Image from 'next/image' 
 import styles from '../styles/Collect.module.css'; 
+import { useContractRead, useContractWrite } from 'wagmi';
+import stakeContractABI from '../components/ABI/stakeContractABI.json'
+import mintContractABI from '../components/ABI/mintContractABI.json'
 
 interface SectionProps {
     stakeContractInstance: ethers.Contract | null;
@@ -14,99 +17,83 @@ interface SectionProps {
 const Collect: React.FC<SectionProps> = ({ stakeContractInstance, mintContractInstance, tokenIds, connectedAddress }) => {
     const [numberOfStakedNFTs, setNumberOfStakedNFTs] = useState<number>(0);
     const[stakedNFT_Ids, setStakedNFT_Ids] = useState<number[]>([]);
-    const[accumulatedRewards, setAccumulatedRewards] = useState<number>(0);
+    const[accumulatedRewards, setAccumulatedRewards] = useState<string>('00.0000');
+    const mintContractAddress = '0x5CD5a6dCf173a4e44CC62dB621C957c4B133E270';
+    const stakeContractAddress = '0xe0833Fba47fAEF2Ea12FEB674B8a2ca98658d1FD';
 
-   
-    const getRewards = useCallback(async (depositNumbers: number[]) => {
-        try {
-          if (!stakeContractInstance) {
-            console.error('Stake contract instance is null');
-            return;
-          }
-    
-          const rewardsArray = await stakeContractInstance.calculateRewards(
-            connectedAddress,
-            depositNumbers
-          );
-    
-          if (rewardsArray && rewardsArray.length > 0) {
-            let sum = BigInt(0);
-            for (let i = 0; i < rewardsArray.length; i++) {
-              sum += BigInt(rewardsArray[i]);
+    const { data, isError, isLoading } = useContractRead({
+        address: stakeContractAddress,
+        abi: stakeContractABI,
+        functionName: 'depositsOf',
+        args:[connectedAddress],
+        enabled: connectedAddress !== null && connectedAddress !== undefined,
+       
+      })
+      useEffect(() => {
+        if (data && Array.isArray(data)) {
+          const numbers = data.map((bigNumber: BigInt) => Number(bigNumber));
+          setStakedNFT_Ids(numbers);
+        }
+        
+      }, [data]);
+
+      const { data: data2, isError: isError2, isLoading: isLoading2 } = useContractRead({
+        address: stakeContractAddress,
+        abi: stakeContractABI,
+        functionName: 'calculateRewards',
+        args:[connectedAddress, stakedNFT_Ids],
+        enabled: connectedAddress !== null && connectedAddress !== undefined,
+       
+      })
+
+      useEffect(() => {
+        const calculateRewards = () => {
+            if (data2 && Array.isArray(data2)) {
+              let sum = BigInt(0);
+              for (let i = 0; i < data2.length; i++) {
+                sum += data2[i];
+              }
+      
+              const sumInEther = formatEther(sum);
+              const sumAsNumber = parseFloat(sumInEther).toFixed(4); // Keeps up to 4 decimals
+              console.log('Total Rewards: ', sumAsNumber);
+              console.log('data2',data2)
+              setAccumulatedRewards(sumAsNumber)
             }
-            let sumInEther = ethers.formatEther(sum.toString());
-            sumInEther = parseFloat(sumInEther).toFixed(4);
-            console.log('Total Rewards: ', sumInEther);
-            setAccumulatedRewards(Number(sumInEther)); // Convert to a number
-          } else {
-            setAccumulatedRewards(0); // Set as a number
-          }
-        } catch (error) {
-          console.error('An error occurred: ', error);
-        }
-      }, [stakeContractInstance, connectedAddress]); // set dependencies
-
-    const getDepositsAndRewards = useCallback(async () => {
-        try {
-          if (!stakeContractInstance) {
-            console.error('Stake contract instance is null');
-            return;
-          }
-      
-          const deposits = await stakeContractInstance.depositsOf(connectedAddress);
-          const depositNumbers = deposits.map((deposit: BigInt) => Number(deposit));
-          console.log('staked#s', depositNumbers);
-          setStakedNFT_Ids(depositNumbers);
-          setNumberOfStakedNFTs(depositNumbers.length);
-          console.log(stakedNFT_Ids)
-      
-          // Now call getRewards, passing in depositNumbers
-          if (depositNumbers.length > 0) {
-            getRewards(depositNumbers);
-          }
-        } catch (error) {
-          console.log('An error occurred:', error);
-        }
-      }, [stakeContractInstance, connectedAddress, stakedNFT_Ids, getRewards]);
-
-    useEffect(() => {
-        getDepositsAndRewards();
-        const intervalId = setInterval(getDepositsAndRewards, 60 * 1000);
+        };
+        calculateRewards();
+        const intervalId = setInterval(calculateRewards, 60 * 1000);
         return () => clearInterval(intervalId);
-      }, []);
-
-      async function claimRewards() {
-        try {
-          if (!stakeContractInstance) {
-            console.error('Stake contract instance is null');
-            return;
-          }
-          
-          const transaction = await stakeContractInstance.claimRewards(stakedNFT_Ids, {
-            gasLimit: ethers.toBeHex(600000),
-        });
-          console.log('Transaction: ', transaction);
-          const receipt = await transaction.wait();
-          console.log('Transaction was mined: ', receipt);
-          getRewards(stakedNFT_Ids);  
+    }, [data2]);
     
-        } catch (error) {
-          console.error('An error occurred: ', error);
-        }
+
+        const {data: data3, isLoading: isLoading3, isSuccess: isSuccess3, write: write2} = useContractWrite({
+            address: stakeContractAddress,
+            abi: stakeContractABI,
+            functionName: 'claimRewards',
+            args:[stakedNFT_Ids],
+        })
+useEffect(()=>{
+    if(isSuccess3 === true){
+        setAccumulatedRewards('0.0000')
     }
+},[data3,isSuccess3])
+   
+
+    
     
       
 
   return (
     <div className={styles.boxxx}>
-         <h5>Each NFT recieves {numberOfStakedNFTs} KIWI per day! </h5>
+         <h5>Each NFT recieves 1 KIWI per day! </h5>
       <Image src={nft} alt='nft' width={110} height={200} />
       <h5>Number of CNFTs Staked: {numberOfStakedNFTs} </h5>
       <Image src={rewardsLogo} alt='rewardsLogo' width={150} height={50} />
       <div className={styles.textBox}>
   {accumulatedRewards}
 </div>
-<button id={styles.claimBtn} className={styles.button} onClick={claimRewards}>Claim Rewards</button>
+<button id={styles.claimBtn} className={styles.button} disabled={!write2} onClick={() => write2()}>Claim Rewards</button>
      
 
     </div>
