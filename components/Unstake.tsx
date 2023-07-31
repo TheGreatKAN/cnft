6f91f11a6893c6cdd2f24e16a81c0b6f37440ef0
TheGreatKAN <95988106+TheGreatKAN@users.noreply.github.com>
2023-07-31 03:05:34 -0400

    Update Unstake.tsx

--- components/Unstake.tsx
+++ components/Unstake.tsx
@@ -3,6 +3,9 @@ import { ethers } from 'ethers';
 import styles from '../styles/UnStake.module.css'; 
 import nft from '../images/nftPrev.png'
 import Image from 'next/image' 
+import stakeContractABI from '../components/ABI/stakeContractABI.json'
+import mintContractABI from '../components/ABI/mintContractABI.json'
+import { useContractRead, useContractWrite } from 'wagmi';
 
 interface SectionProps {
     stakeContractInstance: ethers.Contract | null;
@@ -13,7 +16,11 @@ interface SectionProps {
 
 const Unstake: React.FC<SectionProps> = ({ stakeContractInstance, mintContractInstance, tokenIds, connectedAddress }) => {
     
+    const [numberOfStakedNFTs, setNumberOfStakedNFTs] = useState<number>(0);
     const[stakedNFT_Ids, setStakedNFT_Ids] = useState<number[]>([]);
+    
+    const mintContractAddress = '0x5CD5a6dCf173a4e44CC62dB621C957c4B133E270';
+    const stakeContractAddress = '0xe0833Fba47fAEF2Ea12FEB674B8a2ca98658d1FD';
     const [selectedTokenIds, setSelectedTokenIds] = useState<number[]>([]);
     
 
@@ -47,46 +54,37 @@ const Unstake: React.FC<SectionProps> = ({ stakeContractInstance, mintContractIn
         );
     };
 
-    useEffect(() => {
-        async function getDeposits() {
-          try {
-            if (stakeContractInstance) {
-              const deposits = await stakeContractInstance.depositsOf(connectedAddress);
-              const depositNumbers = deposits.map((deposit: BigInt) => Number(deposit));
-
-              console.log('staked#s', depositNumbers);
-              setStakedNFT_Ids(depositNumbers);
-            }
-          } catch (error) {
-            console.log('An error occurred:', error);
-          }
+    const { data, isError, isLoading } = useContractRead({
+        address: stakeContractAddress,
+        abi: stakeContractABI,
+        functionName: 'depositsOf',
+        args:[connectedAddress],
+        enabled: connectedAddress !== null && connectedAddress !== undefined,
+       
+      })
+      useEffect(() => {
+        if (data && Array.isArray(data)) {
+          const numbers = data.map((bigNumber: BigInt) => Number(bigNumber));
+          setStakedNFT_Ids(numbers);
         }
-        getDeposits();
-      }, [connectedAddress, stakeContractInstance]);
+        
+      }, [data]);
+
+      const {data: data3, isLoading: isLoading3, isSuccess: isSuccess3, write: write2} = useContractWrite({
+        address: stakeContractAddress,
+        abi: stakeContractABI,
+        functionName: 'withdraw',
+        args:[stakedNFT_Ids],
+    })
+
+    useEffect(()=>{
+        if(isSuccess3 === true){
+        setStakedNFT_Ids(prevNFTIds => prevNFTIds.filter(id => !selectedTokenIds.includes(id)));
+    }
+    },[data3, isSuccess3])
       
 
-      const unstakeTokens = async (selectedTokenIds: number[]) => {
-        try {
-            if (!Array.isArray(selectedTokenIds) || selectedTokenIds.length === 0) {
-                alert('Please select the NFTs you want to unstake');
-                return;
-            }
-            if (!stakeContractInstance) {
-                console.error('Stake contract instance not available');
-                return;
-            }
-            const transaction = await stakeContractInstance.withdraw(selectedTokenIds, {
-                gasLimit: ethers.toBeHex(600000),
-            });
-            await transaction.wait();
-            console.log('Unstake successful for tokens: ', selectedTokenIds);
-            setStakedNFT_Ids(prevNFTIds => prevNFTIds.filter(id => !selectedTokenIds.includes(id)));
-        } catch (error) {
-            console.error('Error unstaking tokens: ', error);
-        }
-    };
-    
-
+   
     
   return (
     <div className={styles.boxx}>
@@ -97,7 +95,7 @@ const Unstake: React.FC<SectionProps> = ({ stakeContractInstance, mintContractIn
     <div className={styles.scrollBox} style={{ overflowY: 'scroll', maxHeight: '50%', width:'100%' }}>
         {stakedNFT_Ids.map(tokenId => renderToken(tokenId))}
     </div>
-    <button id={styles.stakeButton} className={styles.button} onClick={() => unstakeTokens(selectedTokenIds)}>
+    <button id={styles.stakeButton} className={styles.button}  disabled={!write2} onClick={() => write2()}>
             UnStake Selected
         </button>
 
